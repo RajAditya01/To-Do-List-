19f97d7878f9d0e2e8df54a800b587eeacfdacbf
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2023-09-22 23:05:54 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,45 @@
-import react from "react";
-const App = () =>{
-    return (
-        <>
-        <div className="main_div">
-            <div className="center-div">
-                <br/>
-                <h1>ToDo List</h1>
-                <br/>
-                <input type="text" placeholder="Add  a Items" />
-                <button> + </button>
+import React, { useState } from "react";
+import ToDoList from "./ToDoList";
 
-                <ol>
-                    <li> buy apple </li>
-                </ol>
-            </div>
+const App = () => {
+  const [inputList, setInputList] = useState("");
+  const [items, setItems] = useState([]);
+
+  const itemEvent = (event) => {
+    setInputList(event.target.value);
+  };
+
+  const listOfItems = () => {
+    setItems((oldItems) => {
+      return [...oldItems, inputList];
+    });
+    setInputList(""); // Clear the input field after adding an item
+  };
+
+  return (
+    <>
+      <div className="main_div">
+        <div className="center-div">
+          <br />
+          <h1>ToDo List</h1>
+          <br />
+          <input
+            type="text"
+            placeholder="Add an Item"
+            onChange={itemEvent}
+            value={inputList}
+          />
+          <button onClick={listOfItems}> + </button>
+
+          <ol>
+            {items.map((itemValue, index) => (
+              <ToDoList key={index} item={itemValue} />
+            ))}
+          </ol>
         </div>
-        </>
-    );
+      </div>
+    </>
+  );
 };
 
 export default App;
---
+++ src/ToDoList.jsx
@@ -0,0 +1,7 @@
+import rect from 'react';
+
+const ToDoList = () =>{
+    return <li> {itemval} </li>
+}
+
+export default ToDoList;
