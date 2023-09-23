883df980a10c1644050e4289157b4354c82fa451
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2023-09-23 11:56:30 +0530

    .

--- src/IncrDec.jsx
+++ src/IncrDec.jsx
@@ -1,14 +1,28 @@
-import react from 'react';
+import react, { useState } from 'react';
 
 const IncrDec = () => {
+const incNum = () =>{
+    setNum(num+1);
+}
+
+const decNum = () => {
+    if(num > 0){
+        setNum(num-1);
+    }else{
+        alert("Sorry Zero limit Reached");
+        setNum(0);
+    }
+}
+
+    const [num, setNum] = useState(0);
     return(
         <>
         <div className='main-div'>
             <div className='center_div'>
-                <h1> 0 </h1>
+                <h1> {num} </h1>
                 <div className='btn_div' >
-                    <button> Increment </button>
-                    <button> Decrement </button>
+                    <button onClick={incNum}> Increment </button>
+                    <button onClick={decNum}> Decrement </button>
                 </div>
             </div>
         </div>
