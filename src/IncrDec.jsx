d830d3371be33cb38f3ca7693539253b0c799bef
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2023-09-23 10:51:18 +0530

    .

---
+++ src/IncrDec.jsx
@@ -0,0 +1,19 @@
+import react from 'react';
+
+const IncrDec = () => {
+    return(
+        <>
+        <div className='main-div'>
+            <div className='center_div'>
+                <h1> 0 </h1>
+                <div className='btn_div' >
+                    <button> Increment </button>
+                    <button> Decrement </button>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default IncrDec;
